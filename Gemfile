03f8b7489ebc9a3aaaa9c892893c1043d1f18042
chanweiyan <chanweiyan007@gmail.com>
2024-03-21 11:56:42 +0800

    fix: update ruby verison

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 # source "https://rubygems.org"
 source 'https://gems.ruby-china.com'
 
-ruby "3.0.0"
+ruby "3.3.0"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.1.3", ">= 7.1.3.2"
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,20 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(title: '....', body: '....')
+    if @article.save
+      flash[:success] = "Article successfully created"
+      redirect_to @article
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+
 end
