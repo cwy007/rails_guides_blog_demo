9b6c7ad7c611f98969a4adb0cd509ce8f3267d69
chanweiyan <chanweiyan007@gmail.com>
2024-03-25 10:15:16 +0800

    feat: implement create update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,14 +12,31 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(title: '....', body: '....')
+    @article = Article.new(article_params)
     if @article.save
-      flash[:success] = "Article successfully created"
+      # flash[:success] = "Article successfully created"
       redirect_to @article
     else
-      flash[:error] = "Something went wrong"
-      render 'new'
+      # flash[:error] = "Something went wrong"
+      render :new, status: :unprocessable_entity
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
